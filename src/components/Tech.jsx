646ff51ea6528b502758614e99394d9b8ef8695f
gunjan1909 <bhanarkaranita@gmail.com>
2023-04-17 01:37:16 +0530

    .

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -24,7 +24,7 @@ const Tech = () => {
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div
-            className="w-28 h-28 flex items-center justify-center rounded-full bg-tertiary shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-300"
+            className="w-28 h-28 flex items-center justify-center rounded-full bg-tertiary shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-300 flex-col"
             key={technology.name}
           >
             <div className="w-20 h-20 rounded-full bg-primary flex items-center justify-center transform hover:rotate-12 transition-all duration-300">
