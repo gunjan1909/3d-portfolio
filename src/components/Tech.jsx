c488e2b28e5dd63c9023b3a67e4c05eb69d9f4b0
gunjan1909 <bhanarkaranita@gmail.com>
2023-04-17 02:07:05 +0530

    testtesttest

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -13,15 +13,15 @@ const Tech = () => {
         <p className={styles.sectionSubText}>What i generally work on.</p>
         <h2 className={styles.sectionHeadText}>Technologies</h2>
       </motion.div>
-      {/* <div className="mt-10 flex flex-row flex-wrap justify-center gap-10">
+      <div className="mt-10 flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
             <BallCanvas icon={technology.icon} />
             <p className="text-center">{technology.name}</p>
           </div>
         ))}
-      </div> */}
-      <div className="flex flex-row flex-wrap justify-center gap-10">
+      </div>
+      {/* <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div
             className="w-28 h-28 flex items-center justify-center rounded-full bg-tertiary shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-300 flex-col"
@@ -37,7 +37,7 @@ const Tech = () => {
             <p className="text-center">{technology.name}</p>
           </div>
         ))}
-      </div>
+      </div> */}
     </>
   );
 };
