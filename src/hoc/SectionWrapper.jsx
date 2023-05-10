103d1da3c435770db602eee7a203aa4831e14151
gunjan1909 <bhanarkaranita@gmail.com>
2023-05-10 12:22:12 +0530

    .

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
+//higher order component(hoc)
 const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
